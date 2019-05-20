4e312f1c376a0cc95748cbdaeee958f0b275354e
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-20 22:07:20 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -2,6 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
 	public function index()
 	{
 		$this->load->view('templates/auth_header');
@@ -11,8 +15,15 @@ class Auth extends CI_Controller {
 
 	public function daftar()
 	{
-		$this->load->view('templates/auth_header');
-		$this->load->view('auth/daftar.php');
-		$this->load->view('templates/auth_footer');
+		$this->form_validation->set_rules('nama','Nama','required|trim');
+		$this->form_validation->set_rules('email','Email','required|trim|valid_email');
+		if($this->form_validation->run()==false){
+			$data['title']='Al - Barokah Shop';
+			$this->load->view('templates/auth_header',$data);
+			$this->load->view('auth/daftar.php');
+			$this->load->view('templates/auth_footer');
+	}else{
+		echo 'Data berhasil ditambahkan';
+	}
 	}
 }
