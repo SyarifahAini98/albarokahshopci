21f32a62938b97f4cd03e87f31d7fb948bcad85d
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-20 22:44:31 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -8,6 +8,7 @@ class Auth extends CI_Controller {
 	}
 	public function index()
 	{
+		$data['title']='Masuk Pelanggan';
 		$this->load->view('templates/auth_header');
 		$this->load->view('auth/masuk.php');
 		$this->load->view('templates/auth_footer');
@@ -16,7 +17,9 @@ class Auth extends CI_Controller {
 	public function daftar()
 	{
 		$this->form_validation->set_rules('nama','Nama','required|trim');
-		$this->form_validation->set_rules('email','Email','required|trim|valid_email|is_unique[pelanggan.email]');
+		$this->form_validation->set_rules('email','Email','required|trim|valid_email|is_unique[pelanggan.email]',[
+			'is_unique'=>'Email ini sudah terdaftar!'
+		]);
 		$this->form_validation->set_rules('password1','Password','required|trim|min_length[3]|matches[password2]',[
 			'matches'=>'Password tidak sama!',
 			'min_length'=>'Password minimal 3 karakter!'
@@ -28,7 +31,18 @@ class Auth extends CI_Controller {
 			$this->load->view('auth/daftar.php');
 			$this->load->view('templates/auth_footer');
 	}else{
-		echo 'Data berhasil ditambahkan';
+		$data=[
+			'nama'=>$this->input->post('nama'),
+			'email'=>$this->input->post('email'),
+			'foto'=>'default_foto_pelanggan.jpg',
+			'password'=>password_hash($this->input->post('password'),PASSWORD_DEFAULT,
+				'role_id'=>2,,
+				'is_active'=>1,
+				'date_created'=>time()
+		];
+		$this->db->insert('pelanggan',$data);
+		$this->session->session->set_flashdata('pesan','<div class="alert alert-success" role="alert">Selamat! akunmu telah dibuat. Silahkan Masuk</div>')
+		redirect('auth');
 	}
 	}
 }
