1fd910f79b8e43f1ff004d98152088e3a34fe0a7
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-20 20:11:15 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Auth extends CI_Controller {
 	public function index()
 	{
+		$this->load->view('template/auth_header');
 		$this->load->view('auth/login.php');
+		$this->load->view('template/auth_footer');
 	}
 }
