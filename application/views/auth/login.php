97c1fc42cf72b38be795cdd13ae9cc84f8a138b9
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-20 20:09:29 +0700

    Update login.php

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -45,16 +45,10 @@
                     <div class="form-group">
                       <input type="password" class="form-control form-control-user" id="password" placeholder="Masukkan Password..." name="password">
                     </div>
-                    <button href="index.html" class="btn btn-primary btn-user btn-block">
+                    <button type="submit" class="btn btn-primary btn-user btn-block">
                       Masuk
                     </button>
                     <hr>
-                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a>
                   </form>
                   <hr>
                   <div class="text-center">
