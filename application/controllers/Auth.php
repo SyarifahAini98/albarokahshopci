4b1199a1e4aeb8aaff005d8fd9d299d71c936310
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-20 22:29:50 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -16,7 +16,12 @@ class Auth extends CI_Controller {
 	public function daftar()
 	{
 		$this->form_validation->set_rules('nama','Nama','required|trim');
-		$this->form_validation->set_rules('email','Email','required|trim|valid_email');
+		$this->form_validation->set_rules('email','Email','required|trim|valid_email|is_unique[pelanggan.email]');
+		$this->form_validation->set_rules('password1','Password','required|trim|min_length[3]|matches[password2]',[
+			'matches'=>'Password tidak sama!',
+			'min_length'=>'Password minimal 3 karakter!'
+		]);
+		$this->form_validation->set_rules('password2','Password','required|trim|matches[password1]');
 		if($this->form_validation->run()==false){
 			$data['title']='Al - Barokah Shop';
 			$this->load->view('templates/auth_header',$data);
