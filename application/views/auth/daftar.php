6fb08e848a0428690a523599939a36978bc16e81
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-21 13:16:50 +0700

    Update daftar.php

--- application/views/auth/daftar.php
+++ application/views/auth/daftar.php
@@ -56,10 +56,7 @@
               </form>
               <hr>
               <div class="text-center">
-                <a class="small" href="forgot-password.html">Lupa Password?</a>
-              </div>
-              <div class="text-center">
-                <a class="small" href="<?= base_url('auth');?>">Sudah Punya Akun? Login!</a>
+                <a class="small" href="<?= base_url('auth');?>">Sudah Punya Akun? Masuk!</a>
               </div>
             </div>
           </div>
