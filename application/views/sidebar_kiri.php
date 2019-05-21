b456c97f354062d104c0a19b5a430138ecc6824a
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-21 20:58:01 +0700

    Create sidebar_kiri.php

---
+++ application/views/sidebar_kiri.php
@@ -0,0 +1,30 @@
+        <div class="row">
+            <div class="col-md-3">
+                <div>
+                    <a href="#" class="list-group-item active">Kategori
+                    </a>
+                    <ul class="list-group">
+                        <li class="list-group-item">Alat Musik
+      <span class="label label-primary pull-right">234</span>
+                        </li>
+                        <li class="list-group-item">Alat Pancing
+                      <span class="label label-success pull-right">34</span>
+                        </li>
+                        <li class="list-group-item">Alat Olahraga
+                         <span class="label label-danger pull-right">4</span>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.div -->
+                <div>
+                    <a href="#" class="list-group-item active list-group-item-success">Testimoni
+                    </a>
+                    <ul class="list-group">
+                        <li class="list-group-item">-
+                             <span class="label label-danger pull-right"> </span>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.div -->
+            </div>
+            <!-- /.col -->
