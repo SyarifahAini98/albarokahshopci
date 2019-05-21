921c863e8a769b89cd578490c871f38c32a258b7
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-21 20:57:43 +0700

    Update Beranda.php

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -2,8 +2,22 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Beranda extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->helper(array('url'));
+		$this->load->model('Model_Produk');
+	}
 	public function index()
 	{
-		$this->load->view('beranda');
+			$data = array(
+				'data1'=>$this->Model_Produk->get_header_produk_terbaru_alat_musik(),
+				'data2'=>$this->Model_Produk->get_header_produk_terbaru_alat_pancing(),
+				'data3'=>$this->Model_Produk->get_header_produk_terbaru_alat_olahraga());
+			$this->load->view('header',$data);
+			$this->load->view('sidebar_kiri');
+			$data = array(
+				'data'=>$this->Model_Produk->get_produk());
+			$this->load->view('beranda',$data);
+			$this->load->view('footer');
 	}
 }
