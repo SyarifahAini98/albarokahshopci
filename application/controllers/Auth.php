d619d90f9b127c0bd328a949b32fd7f9d0ca97a8
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-21 13:16:38 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -8,13 +8,8 @@ class Auth extends CI_Controller {
 	}
 	public function index()
 	{
-		$this->form_validation->set_rules('nama','Nama','trim|required');
-		$this->form_validation->set_rules('jkel','Jenis Kelamin','trim|required');
 		$this->form_validation->set_rules('email','Email','trim|required|valid_email');
-		$this->form_validation->set_rules('no_telp','No Telp','trim|required');
-		$this->form_validation->set_rules('alamat','Alamat','trim|required');
-		$this->form_validation->set_rules('username','Username','trim|required');
-		$this->form_validation->set_rules('password','Password','trim|required|valid_email');
+		$this->form_validation->set_rules('password','Password','trim|required');
 
 		if($this->form_validation->run()==false){
 			$data['title']='Masuk Pelanggan';
@@ -28,16 +23,24 @@ class Auth extends CI_Controller {
 	}
 
 	private function _login(){
-		$nama=$this->input->post('nama');
-		$jkel=$this->input->post('jkel');
 		$email=$this->input->post('email');
-		$no_telp=$this->input->post('no_telp');
-		$alamat=$this->input->post('alamat');
-		$username=$this->input->post('username');
 		$password=$this->input->post('password');
 		$user=$this->db->get_where('pelanggan',['email'=>$email])->row_array();
-		var_dump($user);
-		die;
+		if($user){
+			if(password_verify($password,$user['password'])){
+				$data=[
+					'email'=>$user['email'],
+				];
+				$this->session->set_userdata($data);
+				redirect('beranda_pelanggan');
+			}else{
+				$this->session->set_flashdata('pesan','<div class="alert alert-danger" role="alert">Password Salah!</div>');
+				redirect('auth');
+			}
+		}else{
+			$this->session->set_flashdata('pesan','<div class="alert alert-danger" role="alert">Email tidak terdaftar!</div>');
+			redirect('auth');
+		}
 	}
 
 	public function daftar()
@@ -65,11 +68,17 @@ class Auth extends CI_Controller {
 			'alamat'=>$this->input->post('alamat'),
 			'username'=>htmlspecialchars($this->input->post('username',true)),
 			'foto'=>'default_foto_pelanggan.jpg',
-			'password'=>password_hash($this->input->post('password'),PASSWORD_DEFAULT)
+			'password'=>password_hash($this->input->post('password1'),PASSWORD_DEFAULT)
 		];
 		$this->db->insert('pelanggan',$data);
 		$this->session->set_flashdata('pesan','<div class="alert alert-success" role="alert">Selamat! akunmu telah dibuat. Silahkan Masuk</div>');
 		redirect('auth');
 	}
+	}
+
+	public function logout(){
+		$this->session->unset_userdata('email');
+		$this->session->set_flashdata('pesan','<div class="alert alert-success" role="alert">Anda sudah keluar!</div>');
+		redirect('auth');
 	}
 }
