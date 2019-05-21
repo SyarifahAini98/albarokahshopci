0ac4bf99a067f7054273ecf5a223e5452a2f7516
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-21 20:57:47 +0700

    Update Beranda_pelanggan.php

--- application/controllers/Beranda_pelanggan.php
+++ application/controllers/Beranda_pelanggan.php
@@ -2,8 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Beranda_pelanggan extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+	}
 	public function index(){
 		$data['user']=$this->db->get_where('pelanggan',['email'=>$this->session->userdata('email')])->row_array();
 		$this->load->view('user/index');
+
 	}
 }
