74466cc44e621bb418f5adf75467084ce397d305
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-21 08:00:18 +0700

    Update daftar.php

--- application/views/auth/daftar.php
+++ application/views/auth/daftar.php
@@ -18,6 +18,10 @@
                   <input type="email" class="form-control form-control-user" id="email" placeholder="Email" name="email" value="<?= set_value('email');?>">
                   <?= form_error('email','<small class="text-danger pl-3">','</small>');?>
                 </div>
+                <div class="form-group">
+                  <input type="text" class="form-control form-control-user" id="username" placeholder="Username" name="username" value="<?= set_value('username');?>">
+                  <?= form_error('nama','<small class="text-danger pl-3">','</small>');?>
+                </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
                     <input type="password" class="form-control form-control-user" id="password1" placeholder="Password" name="password1">
