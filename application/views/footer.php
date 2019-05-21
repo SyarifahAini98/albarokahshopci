986b123cc4e7c24728b82dcae0b799f4a162f07a
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-21 20:58:15 +0700

    Create footer.php

---
+++ application/views/footer.php
@@ -0,0 +1,87 @@
+
+    <div class="col-md-12 download-app-box text-center">
+        Dapatkan Produk - Produk Berkualitas Hanya di Toko <a href="#" class="btn btn-danger btn-lg">Al - Barokah Shop</a>
+    </div>
+
+    <!--Footer -->
+    <div class="col-md-12 footer-box">
+        <div class="row">
+            <div class="col-md-4">
+                <strong>Kirim Saran</strong>
+                <hr>
+                <form>
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" required="required" placeholder="Nama">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" required="required" placeholder="Email">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12">
+                            <div class="form-group">
+                                <textarea name="pesan" id="message" required="required" class="form-control" rows="3" placeholder="Pesan"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Kirim</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="col-md-4">
+                <strong>Lokasi Toko</strong>
+                <hr>
+                <p>
+                    Jalan Raya No. 15 Tempeh - Kab. Lumajang,<br>
+                    Jawa Timur, Indonesia<br>
+                    No. HP : 089695356694<br>
+                    Email : albarokahshop@gmail.com<br>
+                </p>
+
+                © 2019 | Al - Barokah Shop | MIF B 2019
+            </div>
+            <div class="col-md-4 social-box">
+                <strong>Sosial Media</strong>
+                <hr>
+                <a href="#"><i class="fa fa-facebook-square fa-3x "></i></a>
+                <a href="#"><i class="fa fa-twitter-square fa-3x "></i></a>
+                <a href="#"><i class="fa fa-google-plus-square fa-3x c"></i></a>
+                <a href="#"><i class="fa fa-linkedin-square fa-3x "></i></a>
+                <a href="#"><i class="fa fa-pinterest-square fa-3x "></i></a>
+                <p>
+                    Terimakasih telah mengunjungi toko kami.<br>
+                    Semoga Anda puas dengan layanan kami. 
+                </p>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <!-- /.col -->
+    <div class="col-md-12 end-box " align="center">
+        © 2019 | Al - Barokah Shop | MIF B 2019 | Email Kami : albarokahshop@gmail.com
+    </div>
+    <!-- /.col -->
+    <!--Footer end -->
+    <!--Core JavaScript file  -->
+    <script src="<?= base_url('assets/');?>js/jquery-1.10.2.js"></script>
+    <!--bootstrap JavaScript file  -->
+    <script src="<?= base_url('assets/');?>js/bootstrap.js"></script>
+    <!--Slider JavaScript file  -->
+    <script src="<?= base_url('assets/');?>ItemSlider/js/modernizr.custom.63321.js"></script>
+    <script src="<?= base_url('assets/');?>ItemSlider/js/jquery.catslider.js"></script>
+    <script>
+        $(function () {
+
+            $('#mi-slider').catslider();
+
+        });
+      </script>
+</body>
+</html>
