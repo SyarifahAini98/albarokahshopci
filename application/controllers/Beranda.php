62e016daf2cb862053397df74e233bad8e65870f
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-21 21:25:58 +0700

    Update Beranda.php

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -12,10 +12,12 @@ class Beranda extends CI_Controller {
 			$data = array(
 				'data1'=>$this->Model_Produk->get_header_produk_terbaru_alat_musik(),
 				'data2'=>$this->Model_Produk->get_header_produk_terbaru_alat_pancing(),
-				'data3'=>$this->Model_Produk->get_header_produk_terbaru_alat_olahraga());
+				'data3'=>$this->Model_Produk->get_header_produk_terbaru_alat_olahraga(),
+				'data4'=>$this->Model_Produk->get_produk_populer());
 			$this->load->view('header',$data);
 			$this->load->view('sidebar_kiri');
 			$data = array(
+				'data'=>$this->Model_Produk->get_jumlah_produk(),
 				'data'=>$this->Model_Produk->get_produk());
 			$this->load->view('beranda',$data);
 			$this->load->view('footer');
