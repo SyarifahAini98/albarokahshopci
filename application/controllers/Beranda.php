8d4cad4038d0e5b53ef1df11540491e12816c23f
Qomariaq <qomaria.qolbi@gmail.com>
2019-05-22 10:54:59 +0700

    Update Beranda.php

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -43,6 +43,31 @@ class Beranda extends CI_Controller {
 			$this->load->view('footer');
 	}
 
+	public function cara_pemesanan()
+	{
+			$data = array(
+				'data1'=>$this->Model_Produk->get_header_produk_terbaru_alat_musik(),
+				'data2'=>$this->Model_Produk->get_header_produk_terbaru_alat_pancing(),
+				'data3'=>$this->Model_Produk->get_header_produk_terbaru_alat_olahraga(),
+				'data4'=>$this->Model_Produk->get_produk_populer());
+			$this->load->view('header',$data);
+			$this->load->view('sidebar_kiri');
+			$this->load->view('cara_pemesanan',$data);
+			$this->load->view('footer');
+	}
+
+public function tentang_kami()
+	{
+			$data = array(
+				'data1'=>$this->Model_Produk->get_header_produk_terbaru_alat_musik(),
+				'data2'=>$this->Model_Produk->get_header_produk_terbaru_alat_pancing(),
+				'data3'=>$this->Model_Produk->get_header_produk_terbaru_alat_olahraga(),
+				'data4'=>$this->Model_Produk->get_produk_populer());
+			$this->load->view('header',$data);
+			$this->load->view('sidebar_kiri');
+			$this->load->view('tentang_kami',$data);
+			$this->load->view('footer');
+	}
 	//buat fungsi cara_pemesanan dan tentang_kami
 	//buat view cara_pemesanan dan tentang_kami
 
