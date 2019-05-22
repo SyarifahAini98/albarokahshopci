382861658f644888cdc449efba55e7d87aacdda7
Qomariaq <qomaria.qolbi@gmail.com>
2019-05-22 10:27:25 +0700

    Create cara_pemesanan.php

---
+++ application/views/cara_pemesanan.php
@@ -0,0 +1,17 @@
+
+            <div class="col-md-9">
+                <div>
+                    <ol class="breadcrumb">
+                        <li class="active">Beranda</li>
+                        <li><a href="#">Cara Pemesanan</a></li>
+                        <li><a href="#">Tentang Kami</a></li>
+                    </ol>
+                </div>
+                <!-- /.div -->
+                <img src="<?= base_url('assets/');?>img/cara_pemesanan.jpg" alt="" style="height:80px; width:80px;"/>
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
