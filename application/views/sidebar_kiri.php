88a8ab40b7cfda925bb815e8eb8f4e0774777101
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-22 10:11:58 +0700

    Update sidebar_kiri.php

--- application/views/sidebar_kiri.php
+++ application/views/sidebar_kiri.php
@@ -4,15 +4,33 @@
                     <a href="#" class="list-group-item active">Kategori
                     </a>
                     <ul class="list-group">
+                        <?php
+                        foreach ($data1 as $row):
+                        ?>
+                        <a href="<?= base_url('beranda/kategori_alat_musik');?>">
                         <li class="list-group-item">Alat Musik
-      <span class="label label-primary pull-right">234</span>
+      <span class="label label-primary pull-right"><?= $row->total;?></span>
                         </li>
+                        </a>
+                        <?php endforeach;?>
+                        <?php
+                        foreach ($data2 as $row):
+                        ?>
+                        <a href="<?= base_url('beranda/kategori_alat_pancing');?>">
                         <li class="list-group-item">Alat Pancing
-                      <span class="label label-success pull-right">34</span>
+                      <span class="label label-success pull-right"><?= $row->total;?></span>
                         </li>
+                        </a>
+                        <?php endforeach;?>
+                        <?php
+                        foreach ($data3 as $row):
+                        ?>
+                        <a href="<?= base_url('beranda/kategori_alat_olahraga');?>">
                         <li class="list-group-item">Alat Olahraga
-                         <span class="label label-danger pull-right">4</span>
+                         <span class="label label-danger pull-right"><?= $row->total;?></span>
                         </li>
+                        </a>
+                        <?php endforeach;?>
                     </ul>
                 </div>
                 <!-- /.div -->
