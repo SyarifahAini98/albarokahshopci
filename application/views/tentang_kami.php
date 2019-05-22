7d76455282a7531555702b90e3ed7287eed2a1ab
Qomariaq <qomaria.qolbi@gmail.com>
2019-05-22 10:50:54 +0700

    Create tentang_kami.php

---
+++ application/views/tentang_kami.php
@@ -0,0 +1,26 @@
+
+            <div class="col-md-9">
+                <div>
+                    <ol class="breadcrumb">
+                        <li><a href="<?= base_url('beranda');?>">>Beranda</a></li>
+                        <li><a href="<?= base_url('beranda/cara_pemesanan');?>">Cara Pemesanan</a></li>
+                        <li><a class="active">Tentang Kami</a></li>
+                    </ol>
+                </div>
+                <!-- /.div -->
+               <div style="padding-left: 18px;padding-right: 18px;"><br><!-- 
+                        <center><b>Al - Barokah Shop</b></center><br> -->
+                        <center> <img src="<?= base_url('assets/');?>img/tentang_kami.jpg" alt="" style="height:225px; width:550px;"/></center><br>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Al - Barokah Shop adalah sebuah toko yang menyediakan berbagai macam peralatan memancing, olahraga dan juga bebagai keperluan alat musik, bermula dari sebuah oembangunan toko yang berlokasikan di <b>Jalan Raya No. 15 Tempeh - Kab. Lumajang</b>, toko kami akhirnya bisa menjangkau pasar yang lebih luas di internet. Al - Barokah Shop adalah sebuah toko yang didirikan oleh <b>Siti Aminah</b> pada tahun <b>1999</b>.<br><br>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Keberadaan dari toko online ini diharapkan dapat mempermudah Anda untuk mendapatkan berbagai produk musik, olahraga dan juga alat memancing  berkualitas tinggi namun dengan harga yang terjangkau.<br><br>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dalam memberi layanan, kami selalu mencoba memberi persembahan terbaik kepada siapapun. Selain itu kami juga selalu menjunjung tinggi nilai-nilai etika yang baik, seperti kejujuran, ketepatan, dan profesionalitas dalam berbisnis. Mudah-mudahan dengannya toko online kami bisa memberi banyak manfaat bagi Anda.<br><br>
+                        <u>Kontak</u><br>
+                        Email : albarokahshop@gmail.com<br>
+                        No. HP : 089695356694
+                        <br><br>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sekian dulu perkenalan ini. Semoga perkenalan ini bisa memberi inspirasi dan manfaat bagi Anda.<br><br>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Salam dari kami semoga anda sukses selalu.
+                        </div> 
+            </div>
+            <!-- /.col -->
+        
