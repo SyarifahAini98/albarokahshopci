85cb0d2eb9085cc656f6b2c2f909acf499c8fbff
Qomariaq <qomaria.qolbi@gmail.com>
2019-05-22 10:50:50 +0700

    Update cara_pemesanan.php

--- application/views/cara_pemesanan.php
+++ application/views/cara_pemesanan.php
@@ -2,13 +2,13 @@
             <div class="col-md-9">
                 <div>
                     <ol class="breadcrumb">
-                        <li class="active">Beranda</li>
-                        <li><a href="#">Cara Pemesanan</a></li>
+                        <li href="#">Beranda</li>
+                        <li><a class="active">Cara Pemesanan</a></li>
                         <li><a href="#">Tentang Kami</a></li>
                     </ol>
                 </div>
                 <!-- /.div -->
-                <img src="<?= base_url('assets/');?>img/cara_pemesanan.jpg" alt="" style="height:80px; width:80px;"/>
+                <img src="<?= base_url('assets/');?>img/cara_pemesanan.jpg" alt="" style="height:280px; width:650px;"/>
             </div>
             <!-- /.col -->
         </div>
