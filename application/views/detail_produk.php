4c0a70f97ce515e5727161df070dc07d4efd12df
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-05-22 21:08:09 +0700

    Update detail_produk.php

--- application/views/detail_produk.php
+++ application/views/detail_produk.php
@@ -2,7 +2,7 @@
             <div class="col-md-9">
                 <div>
                     <ol class="breadcrumb">
-                        <li>Beranda</li>
+                        <li><a href="<?= base_url('beranda');?>">Beranda</a></li>
                         <li><a href="<?= base_url('beranda/cara_pemesanan');?>">Cara Pemesanan</a></li>
                         <li><a href="<?= base_url('beranda/tentang_kami');?>">Tentang Kami</a></li>
                     </ol>
@@ -13,18 +13,16 @@
                     $no=1;
                     foreach ($data as $row):
                     ?>
-                    <div class="">
+                    <div class="col-md-12 text-center col-sm-6 col-xs-6">
                         <div class="thumbnail product-box" style="height: 300px;">
                             <center>
                             <table border="0" width="100%" style="padding: 5px;border-spacing:5px;border-collapse: separate;">
-                                <tr><td width="35%" rowspan="11"><!-- 
+                                <tr><td width="35%" rowspan="10"><!-- 
                                     <div class="thumbnail product-box"> -->
                                         <center>
-                                        <img src="<?= base_url('assets/');?>img/images_produk/<?= $row->foto;?>" style="height:175px; width:175px;"></center><!-- 
+                                        <img src="<?= base_url('assets/');?>img/images_produk/<?= $row->foto;?>" style="height:175px; width:175px;"><br><h4><b><?= $row->nama_produk;?></b></h4></center><!-- 
                                     </div> -->
                                     </td>
-                                    <td colspan="3"><h4><b><?= $row->nama_produk;?></b></h4></td></tr>
-                                <tr><td colspan="3">&nbsp;</td></tr>
                                 <tr><td width="15%">Merek</td><td width="4%">:</td><td><?= $row->merek;?></td></tr>
                                 <tr><td>Ukuran</td><td>:</td><td>
                                 <?php
@@ -46,31 +44,11 @@
                                 </tr>
                             </table>
                             </center>
-                            dsdsdsds
-                            <img src="<?= base_url('assets/');?>img/images_produk/<?= $row->foto;?>" alt="" style="height:80px; width:80px;"/>
-                            <div class="caption">
-                                <h3><a href="#"><?= $row->nama_produk;?></a></h3>
-                                <p>Harga : <strong>Rp <?= $row->harga_produk;?></strong>  </p>
-                                <p><a href="#"><?= $row->merek;?></a></p>
-                                <p><a href="<?= base_url('auth');?>" class="btn btn-success" role="button">Beli</a> <a href="<?= base_url('beranda/detail_produk/'.$row->id_produk);?>" class="btn btn-primary" role="button">Lihat</a></p>
-                            </div>
                         </div>
                     </div>
                     <?php $no++;
                     endforeach;?>
                     <!-- /.col -->
-                </div>
-                <!-- /.row -->
-                <div class="row">
-                    <ul class="pagination alg-right-pad">
-                        <li><a href="#">&laquo;</a></li>
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                        <li><a href="#">&raquo;</a></li>
-                    </ul>
                 </div>
                 <!-- /.row -->
             </div>
