ef0346540893ddf1ac17cae46f8862d3512ec632
robbykevin <49887792+robbykevin@users.noreply.github.com>
2019-05-23 15:46:43 +0700

    Update Beranda.php

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -6,6 +6,7 @@ class Beranda extends CI_Controller {
 		parent::__construct();
 		$this->load->helper(array('url'));
 		$this->load->model('Model_Produk');
+		$this->load->model('Model_pelanggan');
 	}
 
 	public function index()
@@ -378,6 +379,24 @@ class Beranda extends CI_Controller {
 			$data = array(
 				'data'=>$this->Model_Produk->get_detail_produk($id));
 			$this->load->view('detail_produk',$data);
+			$this->load->view('footer');
+	}
+	public function profil_pelanggan()
+	{
+			$data = array(
+				'data1'=>$this->Model_Produk->get_header_produk_terbaru_alat_musik(),
+				'data2'=>$this->Model_Produk->get_header_produk_terbaru_alat_pancing(),
+				'data3'=>$this->Model_Produk->get_header_produk_terbaru_alat_olahraga(),
+				'data4'=>$this->Model_Produk->get_produk_header_populer());
+			$this->load->view('header',$data);
+			$data = array(
+				'data1'=>$this->Model_Produk->get_jumlah_produk_alat_musik(),
+				'data2'=>$this->Model_Produk->get_jumlah_produk_alat_pancing(),
+				'data3'=>$this->Model_Produk->get_jumlah_produk_alat_olahraga());
+			$this->load->view('sidebar_kiri',$data);
+			$data = array(
+				'data'=>$this->Model_pelanggan-> get_profil_pelanggan());
+			$this->load->view('profil_pelanggan',$data);
 			$this->load->view('footer');
 	}
 
