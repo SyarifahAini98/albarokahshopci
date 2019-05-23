5e5079ce07e82e66dbe3deabf6721982a70cce79
robbykevin <49887792+robbykevin@users.noreply.github.com>
2019-05-23 15:47:04 +0700

    Update Model_pelanggan.php

--- application/models/Model_pelanggan.php
+++ application/models/Model_pelanggan.php
@@ -4,8 +4,8 @@ class Model_pelanggan extends CI_model{
     function get_table(){
         return $this->db->get("pelanggan");
     }
-    function get_pelanggan(){
-        $query = $this->db->query("SELECT * FROM pelanggan");
+    function get_profil_pelanggan(){
+        $query = $this->db->query("SELECT * FROM pelanggan WHERE id_pelanggan='5'");
         return $query->result();
     }
 }
