a12c94ac92e3b96b8b7ec1ae6d7c44076ec3d329
robbykevin <49887792+robbykevin@users.noreply.github.com>
2019-05-23 15:53:44 +0700

    Update Beranda_pelanggan.php

--- application/controllers/Beranda_pelanggan.php
+++ application/controllers/Beranda_pelanggan.php
@@ -10,4 +10,22 @@ class Beranda_pelanggan extends CI_Controller {
 		$this->load->view('user/index');
 
 	}
+	public function profil_pelanggan()
+	{
+			$data = array(
+				'data1'=>$this->Model_Produk->get_header_produk_terbaru_alat_musik(),
+				'data2'=>$this->Model_Produk->get_header_produk_terbaru_alat_pancing(),
+				'data3'=>$this->Model_Produk->get_header_produk_terbaru_alat_olahraga(),
+				'data4'=>$this->Model_Produk->get_produk_header_populer());
+			$this->load->view('header',$data);
+			$data = array(
+				'data1'=>$this->Model_Produk->get_jumlah_produk_alat_musik(),
+				'data2'=>$this->Model_Produk->get_jumlah_produk_alat_pancing(),
+				'data3'=>$this->Model_Produk->get_jumlah_produk_alat_olahraga());
+			$this->load->view('sidebar_kiri',$data);
+			$data = array(
+				'data'=>$this->Model_pelanggan-> get_profil_pelanggan());
+			$this->load->view('profil_pelanggan',$data);
+			$this->load->view('footer');
+	}
 }
