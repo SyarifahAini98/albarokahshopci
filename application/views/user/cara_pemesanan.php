2dd79bf533133e97396d943e081055f9446a0968
robbykevin <49887792+robbykevin@users.noreply.github.com>
2019-05-28 17:54:23 +0700

    Create cara_pemesanan.php

---
+++ application/views/user/cara_pemesanan.php
@@ -0,0 +1,21 @@
+
+            <div class="col-md-9">
+                <div>
+                    <ol class="breadcrumb">
+                    <li><a href="<?= base_url('beranda_pelanggan');?>">Beranda</a></li>
+                        <li>Cara Pemesanan</li>
+                        <li><a href="<?= base_url('beranda_pelanggan/tentang_kami');?>">Tentang Kami</a></li>
+                        <li><a href="<?= base_url('beranda_pelanggan/profil_pelanggan');?>">Profil_Pelanggan</a></li>
+                        
+                    </ol>
+                </div>
+                <!-- /.div -->
+                <br>
+                <center><img src="<?= base_url('assets/');?>img/cara_pemesanan.jpg" alt="" width="645px"/></center>
+                <br><br>
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
