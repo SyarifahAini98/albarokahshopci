3f7da70162ea5ed8e0e00e6264fabfa4ed8387cf
robbykevin <49887792+robbykevin@users.noreply.github.com>
2019-05-28 17:55:03 +0700

    Create kategori_alat_olahraga.php

---
+++ application/views/user/kategori_alat_olahraga.php
@@ -0,0 +1,74 @@
+
+            <div class="col-md-9">
+                <div>
+                    <ol class="breadcrumb">
+                        <li><a href="<?= base_url('beranda_pelanggan');?>">Beranda</a></li>
+                        <li><a href="<?= base_url('beranda_pelanggan/cara_pemesanan');?>">Cara Pemesanan</a></li>
+                        <li><a href="<?= base_url('beranda_pelanggan/tentang_kami');?>">Tentang Kami</a></li>
+                        <li><a href="<?= base_url('beranda_pelanggan/Profil_pelanggan');?>">Profil Pelanggan</a></li>
+                    </ol>
+                </div>
+                <!-- /.div -->
+                <div class="row">
+                    <div class="btn-group alg-right-pad">
+                        <?php
+                        foreach ($data1 as $row):
+                        ?>
+                        <button type="button" class="btn btn-default"><strong><?= $row->total;?>  </strong>produk</button>
+                        <?php
+                        endforeach;?>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown">
+                                Urutkan Produk &nbsp;
+      <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?= base_url('beranda_pelanggan/harga_terendah_alat_olahraga');?>">Harga Terendah</a></li>
+                                <li class="divider"></li>
+                                <li><a href="<?= base_url('beranda_pelanggan/harga_tertinggi_alat_olahraga');?>">Harga Tertinggi</a></li>
+                                <li class="divider"></li>
+                                <li><a href="<?= base_url('beranda_pelanggan/populer_alat_olahraga');?>">Populer</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+                <div class="row">
+                    <?php
+                    $no=1;
+                    foreach ($data2 as $row):
+                    ?>
+                    <div class="col-md-4 text-center col-sm-6 col-xs-6">
+                        <div class="thumbnail product-box" style="height: 300px;">
+                            <img src="<?= base_url('assets/');?>img/images_produk/<?= $row->foto;?>" alt="" style="height:80px; width:80px;"/>
+                            <div class="caption">
+                                <h3><a href="#"><?= $row->nama_produk;?></a></h3>
+                                <p>Harga : <strong>Rp <?= $row->harga_produk;?></strong>  </p>
+                                <p><a href="#"><?= $row->merek;?></a></p>
+                                <p><a href="<?= base_url('auth');?>" class="btn btn-success" role="button">Beli</a> <a href="#" class="btn btn-primary" role="button">Lihat</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <?php $no++;
+                    endforeach;?>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+                <div class="row">
+                    <ul class="pagination alg-right-pad">
+                        <li><a href="#">&laquo;</a></li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li><a href="#">&raquo;</a></li>
+                    </ul>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
