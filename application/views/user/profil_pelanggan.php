85d966cdab27018dba81228a2b56f8403beaa3e5
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-06-06 16:27:41 +0700

    Update profil_pelanggan.php

--- application/views/user/profil_pelanggan.php
+++ application/views/user/profil_pelanggan.php
@@ -1,10 +1,10 @@
 <div class="col-md-9">
                 <div>
                     <ol class="breadcrumb">
-                     <li><a href="<?= base_url('beranda');?>">Beranda</a></li>
-                        <li><a href="<?= base_url('beranda_pelanggan/cara_pemesanan');?>">Cara Pemesanan</a></li>
-                        <li>Profil_Pelanggan</li>
-                        <li><a href="<?= base_url('beranda_pelanggan/tentang_kami');?>">Tentang Kami</a></li>
+                     <li><a href="<?= base_url('beranda_pelanggan');?>">Beranda</a></li>
+                        <li>Profil</li>
+                        <li><a href="<?= base_url('beranda_pelanggan/keranjang');?>">Keranjang</a></li>
+                        <li><a href="<?= base_url('beranda_pelanggan/transaksi');?>">Transaksi</a></li>
                     </ol>
                 </div>
                 <!-- /.div -->
@@ -18,13 +18,13 @@
                                 <tr><td width="35%" rowspan="10"><!-- 
                                     <div class="thumbnail product-box"> -->
                                         <center>
-                                        <img src="<?= base_url('assets/');?>img/pelanggan/<?= $row->foto;?>" style="height:175px; width:175px;"></center><!-- 
+                                        <img src="<?= base_url('assets/');?>img/images_pelanggan/<?= $row->foto;?>" style="height:175px; width:175px;"></center><!-- 
                                     </div> -->
                                     </td>
                                 <tr><td width="15%">Username</td><td width="4%">:</td><td><?= $row->username;?></td></tr>
                                 <tr><td>Email</td><td>:</td><td><strong><font color="orange"> <?= $row->email;?></font></strong></td></tr>
-                                <tr><td>Jeni Kelamin</td><td>:</td><td><?= $row->jkel;?></td></tr>
-                                <tr><td>Nomor Telpon</td><td>:</td><td><?= $row->no_telp;?></td></tr>
+                                <tr><td>Jenis Kelamin</td><td>:</td><td><?= $row->jkel;?></td></tr>
+                                <tr><td>No. Telp</td><td>:</td><td><?= $row->no_telp;?></td></tr>
                                 <tr><td>Alamat</td><td>:</td><td><?= $row->alamat;?></td></tr>
                                 <tr><td colspan="3">&nbsp;</td></tr>
                                 <tr><td colspan="3"></tr>
