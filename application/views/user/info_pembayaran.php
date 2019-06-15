ced7b832f55e3398c3ed0d88195d716bdd7c0b83
Syarifah Aini <35066889+SyarifahAini98@users.noreply.github.com>
2019-06-16 00:12:04 +0700

    Update info_pembayaran.php

--- application/views/user/info_pembayaran.php
+++ application/views/user/info_pembayaran.php
@@ -1,27 +1,29 @@
 
             <div class="col-md-9">
                 <div>
-                    <ol class="breadcrumb">
-                        <li><a href="<?= base_url('beranda_pelanggan');?>">Beranda</a></li>
-                        <li><a href="<?= base_url('beranda_pelanggan/profil_pelanggan');?>">Profil</a></li>
-                        <li><a href="<?= base_url('beranda_pelanggan/keranjang');?>">Keranjang</a></li>
-                        <li><a href="<?= base_url('beranda_pelanggan/transaksi');?>">Transaksi</a></li>
+                    <ol class="breadcrumb" style="text-align: center;">
+                        <li><center><a href="#">Info Pembayaran</a></center></li>
                     </ol>
                 </div>
                 <!-- /.div -->
                 <div class="row">
                     <div class="col-md-12 text-center col-sm-6 col-xs-6">
                         <!-- <div class="thumbnail product-box" style="height: 300px;"> -->
-                        <a href="javascript: history.go(-1)" class="btn btn-default" role="button" style="float:left;">Kembali</a>
                             <center>
                             <table border="0" width="100%" style="padding: 5px;border-spacing:5px;border-collapse: separate;">
                                 <tr><td width="35%" rowspan="10">
-                                    Bukti transfer akan segera dicek dalam waktu 24 jam setelah bukti transfer diuploud.
-1. Gunakan ATM/iBanking/setor tunai untuk transfer ke rekening kami berikut ini :
-No. Rekening : 0021-01-163277-50-0
-Nama Rekening :
-2. Silahkan uploud bukti transfer, kami memberikan waktu 1x24 jam setelah proses checkout.
-3. Demi keamanan, mohon tidak memberikan bukti atau konfirmasi pembayaran pesanan kepada siapapun.
+                                    <center>
+                                    <table width="100%" border="0" style="padding: 5px;border-spacing:5px;border-collapse: separate;">
+                                      <tr><td><u>Bukti transfer akan segera dicek dalam waktu 24 jam setelah bukti transfer diuploud.</u></td></tr>
+                                      <tr><td>1. Gunakan ATM/iBanking/setor tunai untuk transfer ke rekening kami berikut ini :</td></tr>
+                                      <tr><td>No. Rekening : 0021-01-163277-50-0</td></tr>
+                                      <tr><td>Nama Rekening : Isro'ain Dwi Oktaviani</td></tr>
+                                      <tr><td>2. Silahkan uploud bukti transfer, kami memberikan waktu 1x24 jam setelah proses checkout.</td></tr>
+                                      <tr><td>3. Demi keamanan, mohon tidak memberikan bukti atau konfirmasi pembayaran pesanan kepada siapapun.</td></tr>
+                                    </td></tr>
+                                    <tr><td><center><a href="<?= base_url('beranda_pelanggan');?>"><button type="button" class="btn btn-default">Lanjut Belanja</button></a>&nbsp;&nbsp;&nbsp;<a href="<?= base_url('beranda_pelanggan/transaksi');?>"><button type="button" class="btn btn-primary">Upload Bukti Transfer</button></a></center></td></tr>
+                                    </table>
+                                    </center>
                                     </td>
                                 </tr>
                             </table>
